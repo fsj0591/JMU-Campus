5987a9d4c57649686e2d0e2a57e79913ec68763b
fsj0591 <994763265@qq.com>
2023-08-16 17:39:05 +0800

    comment bug修改

--- comment-service/comment-server/src/main/java/com/xueyu/comment/listener/PostMqListener.java
+++ comment-service/comment-server/src/main/java/com/xueyu/comment/listener/PostMqListener.java
@@ -2,6 +2,7 @@ package com.xueyu.comment.listener;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.xueyu.comment.mapper.CommentMapper;
+import com.xueyu.comment.mapper.LikeMapper;
 import com.xueyu.comment.pojo.domain.Comment;
 import com.xueyu.comment.sdk.dto.CommentDTO;
 import com.xueyu.post.sdk.dto.PostOperateDTO;
@@ -15,6 +16,9 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.xueyu.comment.sdk.constant.CommentMqContants.COMMENT_DELETE_KEY;
 import static com.xueyu.comment.sdk.constant.CommentMqContants.COMMENT_EXCHANGE;
@@ -38,6 +42,9 @@ public class PostMqListener {
 	CommentMapper commentMapper;
 
 	@Resource
+	LikeMapper likeMapper;
+
+	@Resource
 	RabbitTemplate rabbitTemplate;
 
 	@RabbitListener(bindings = @QueueBinding(
@@ -50,6 +57,12 @@ public class PostMqListener {
 		// 删除对应的评论
 		LambdaQueryWrapper<Comment> wrapper = new LambdaQueryWrapper<>();
 		wrapper.eq(Comment::getPostId, postOperateDTO.getPostId());
+		List<Comment> list = commentMapper.selectList(wrapper);
+		List<Integer> commentId = new ArrayList<>();
+		for(Comment c : list){
+			commentId.add(c.getId());
+		}
+		likeMapper.deleteBatchIds(commentId);
 		int deleteNum = commentMapper.delete(wrapper);
 		// 删除的数量，发送mq
 		CommentDTO commentDTO = new CommentDTO();
